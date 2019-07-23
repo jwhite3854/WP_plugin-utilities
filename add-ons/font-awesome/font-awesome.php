b963664a34a7ce852e41b42b94bd42e29e3a69de
Julia White <jwhite3854@gmail.com>
2019-07-23 09:41:59 -0500

    added actions to remove core emoji scripts

--- add-ons/font-awesome/font-awesome.php
+++ add-ons/font-awesome/font-awesome.php
@@ -14,3 +14,10 @@ class JWhite_Addons_Font_Awesome
 
 add_action('wp_enqueue_scripts', array( 'JWhite_Addons_Font_Awesome', 'enqueue_font_awesome' ) );
 add_action('admin_enqueue_scripts', array( 'JWhite_Addons_Font_Awesome', 'enqueue_font_awesome' ) );
+
+
+// Remove emoji scripts
+remove_action( 'wp_head', 'print_emoji_detection_script', 7);
+remove_action( 'wp_print_styles', 'print_emoji_styles', 20);
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script', 20);
+remove_action( 'admin_print_styles', 'print_emoji_styles', 20);
